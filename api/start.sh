2a1a97076e8d2d5d29ac69272ed0636c439da6ec
Yellowman <marc.inthaamnouay@gmail.com>
2019-02-08 15:23:00 +0100

    :construction: testing k8s & new docker config

--- api/start.sh
+++ api/start.sh
@@ -3,5 +3,23 @@
 # Install project dependencies
 go get ./...
 
+# Activate the cron on the same container
+cd /task
+
+# Set right on cronjob
+chmod 755 bobba-cron.txt
+
+# Add log to crontab
+touch /var/log/bobba-cron.log
+
+# Add crontab.txt
+/usr/bin/crontab bobba-cron.txt
+
+# Run the cron
+crond
+
+# Go back to task folder
+cd /task
+
 # Start and watch the project
 CompileDaemon -command="./data"
--- cron/startup.sh
+++ cron/startup.sh
@@ -1,13 +1,3 @@
-
-# Install dependencies for the bruteforce.go
-cd bruteforce && go get ./... && cd ..
-
-# Install dependencies for select.go
-cd select && go get ./... && cd ..
-
-# Install dependencies for temporary.go
-cd temporary && go get ./... && cd ..
-
 # Run the crontab only in the context of Docker
 if [ "$1" = "docker" ] 
 then
